27339f7e4006cb3cbd0f3a5784d9a9cd0fa11192
Josh Gagnon <jgagnon@rsa.com>
2016-01-15 20:18:46 -0500

    Skeleton class from example with Iterable instead.

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+
+/** A service that counts words in books. */
+public class WordCount {
+    /**
+     * Count all words in a given book in preparation for later querying.
+     *
+     * @param bookTitle The title of the book. Must be a non-empty String.
+     * @param lines     The book's text. Must not be null.
+     */
+    public void countWords(String bookTitle, Iterable<String> lines) {
+    }
+
+    /**
+     * Provides the top ten most common words found cumulatively across all
+     * books that have been processed by the service.
+     *
+     * @return A mapping of at most ten words to their respective counts. Never
+     *         null, but possibly empty.
+     */
+    public Map<String, Integer> topTenWords() {
+        return null;
+    }
+
+    /**
+     * Provides the top ten most common words found in a book that was already
+     * processed by the service.
+     *
+     * @param bookTitle The title of the book. Must be a non-empty String.
+     * @return A mapping of at most ten words to their respective counts. Never
+     *         null, but possibly empty.
+     */
+    public Map<String, Integer> topTenWords(String bookTitle) {
+        return null;
+    }
+}
